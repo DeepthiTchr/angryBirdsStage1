b683ec23e58f798d70b9adfef0995d6d65854cea
Deepthi <70970105+DeepthiTchr@users.noreply.github.com>
2021-05-15 16:00:11 +0530

    Update sketch.js
    changes in numbers

--- sketch.js
+++ sketch.js
@@ -3,33 +3,31 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1,box2;
+var bird;
+var pig;
+var lohg1
 
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
 
-    
+    box1 = new Box(820,300,70,70);
+    pig1 = new Pig(900,300);
+    box2 = new Box(980,300,70,70);
+    log1 = new Log(900,250,300,PI/2)
+    box3= new Box(820,200,70,70)
+    pig2 = new Pig(900,200)
+    box4= new Box(980,200,70,70)
+    log2= new Log(900,150,300,PI/2)
+    log3= new Log(980,70,150,-PI/7)
+    log4=new Log(860,70,150,PI/7)
+    box5 = new Box(900,100,70,70)
+
     ground = new Ground(600,height,1200,20)
-
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(100,100);
-
+    bird = new Bird(300,200)
+    
 }
 
 function draw(){
@@ -40,18 +38,15 @@ function draw(){
     console.log(box2.body.angle);
     box1.display();
     box2.display();
-    ground.display();
+    bird.display();
     pig1.display();
     log1.display();
-
     box3.display();
     box4.display();
-    pig3.display();
-    log3.display();
-
+    pig2.display();
+    log2.display();
     box5.display();
+    log3.display();
     log4.display();
-    log5.display();
-
-    bird.display();
+    ground.display();
 }
